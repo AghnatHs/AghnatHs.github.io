19bfe6372238400191fcc631695d8599e171707e
AghnatHs <aghnathasya@gmail.com>
2024-01-17 18:37:34 +0700

    first git commit

---
+++ src/hello.js
@@ -0,0 +1,23 @@
+const helloTextElement = document.getElementById("hello-text");
+const greetings = [
+    "Hallo, nama saya",
+    "Hi, my name is",
+    "Hallo, mijn naam is",
+    "Hallo Ich heisse",
+    "Γεια το όνομά μου είναι",
+    "Hej mitt namn är",
+    "Hola, mi nombre es",
+    "こんにちは、私の名前は",
+    "안녕 내 이름은",
+    "Bonjour, je m'appelle"
+];
+
+const maxCounter = greetings.length;
+let counter = 0;
+
+setInterval(function () {
+    counter >= maxCounter - 1 ? counter = 0 : counter += 1;
+    helloTextElement.innerHTML = greetings[counter];
+}, 600);
+
+
