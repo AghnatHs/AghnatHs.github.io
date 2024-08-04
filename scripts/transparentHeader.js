4982d4d884890682221012e818b0ffc26ce9254b
AghnatHs <aghnathasya@gmail.com>
2024-08-04 19:35:38 +0700

    finishing - responsive menu - responsive project section "

--- scripts/transparentHeader.js
+++ scripts/transparentHeader.js
@@ -1,9 +1,13 @@
 const headerElement = document.getElementById('header');
+const width = window.innerWidth;
 
 window.onscroll = () => {
-    if (window.scrollY > 100) {
-        headerElement.classList.add('header-active');
-    } else {
-        headerElement.classList.remove('header-active');
+    if (width > 600) {
+        if (window.scrollY > 100) {
+            headerElement.classList.add('header-active');
+        } else {
+            headerElement.classList.remove('header-active');
+        }
     }
+    console.log(width);
 }
